d07f97509856cca677eae3c4a8d8c7057cb8aaaa
Andres <jmtabares03@gmail.com>
2026-08-18 17:09:41 -0500

    PWA + offline: fotos en IndexedDB, subida automatica con señal

---
+++ sw.js
@@ -0,0 +1,23 @@
+var CACHE_NAME = 'inventario-v1';
+var urlsToCache = ['/inventario-inmobiliario/'];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(urlsToCache);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    fetch(event.request).catch(function() {
+      return caches.match(event.request);
+    })
+  );
+});
